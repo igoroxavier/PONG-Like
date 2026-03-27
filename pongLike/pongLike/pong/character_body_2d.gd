fb99e26df26cb5c432da8b0ae0d4bfcd58a25589
Igor Xavier <igoroliverxavier@gmail.com>
2026-03-26 23:15:32 -0300

    Delete pongLike/pongLike/pong directory

--- pongLike/pongLike/pong/character_body_2d.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("move_up"):
-		self.position.y = self.position.y - 300 * delta
-	if Input.is_action_pressed("move_down"):
-		self.position.y = self.position.y + 300 * delta
--- pongLike/pongLike/pong/gamecontroller.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var player1_points: int = 0
-var player2_points: int = 0
